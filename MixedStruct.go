06620587299a34b1ecb3fa748c4521aa73a8a023
daniel.hitchcock <daniel.hitchcock@centralsquare.com>
2020-10-01 10:22:12 -0400

    Fix datatype for C array (char to long)

--- MixedStruct.go
+++ MixedStruct.go
@@ -12,11 +12,15 @@ import "C"
 
 //export GenStruct
 func GenStruct(length C.int) *C.CStruct {
-	valuesPointer := C.malloc(C.sizeof_char * C.size_t(length))
+	// allocate memory with C.malloc
+	valuesPointer := C.malloc(C.sizeof_long * C.size_t(length))
+	// create an array/slice, such that the C array (values pointer) can be access
 	valuesSlice := (*[1 << 30]C.long)(valuesPointer)[:int(length):int(length)]
+	// assign values to the C array through the slice
 	for i := 0; i < int(length); i++ {
 		valuesSlice[i] = C.long(i)
 	}
+	//make a struct and assign values
 	rtn := (*C.CStruct)(C.malloc(C.sizeof_CStruct))
 	rtn.length = length
 	rtn.data = valuesPointer
--- MixedStruct.py
+++ MixedStruct.py
@@ -4,17 +4,21 @@ from numpy.ctypeslib import ndpointer
 
 GO = CDLL("MixedStruct.so")
 
-
+# Create an object to capture the struct
 class MixedStruct(Structure):
     _fields_ = [("length", c_int), ("data", c_long)]
 
 
+# Define the arg and return types of the function
 GO.GenStruct.argtypes = [c_int]
 GO.GenStruct.restype = POINTER(MixedStruct)
 
 
 def mixed_struct(x):
+    # Get results as the MixedStruct class we defined above
     result = GO.GenStruct(x)
+    # Create a buffer from the memory address contained in the struct as 'data' and
+    # read it as np.int64
     data = np.frombuffer((c_long * x).from_address(result.contents.data), np.int64)
     length = result.contents.length
     return {"Length": length, "Data": data}
